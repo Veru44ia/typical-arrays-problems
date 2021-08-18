40972afe15d24b7965b9bb488ba843f2d391a5f2
Veru44ia <vera3188@gmail.com>
2021-08-18 22:07:50 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (Array.isArray(array) && array.length > 0) {
+    return (array.sort(( a, b ) => a - b)).splice(0, 1);
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (Array.isArray(array) && array.length > 0) {
+    return array.sort(( a, b ) => b-a).splice(0, 1); 
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   
+  if (Array.isArray(array) && array.length > 0) { 
+      result = 0;
+    for (let i = 0; i < array.length; i++) {
+      result += array[i];
+     }
+     return (result / array.length);
+  } else {
+    return 0;
+  } 
 }
